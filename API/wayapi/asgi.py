34684007af349e5ab20e6edfe730bd60a8c48585
OdegaOmie <of78298@yahoo.com>
2020-08-12 12:59:27 +0100

    tested base api

--- API/wayapi/asgi.py
+++ API/wayapi/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for WayApp project.
+ASGI config for wayapi project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WayApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wayapi.settings')
 
 application = get_asgi_application()
